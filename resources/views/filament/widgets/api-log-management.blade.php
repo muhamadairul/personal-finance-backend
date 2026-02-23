169ce9c27205ef5faf21797b470c32cf3f988c8c
muhamadairul <akunpertamapublic@gmail.com>
2026-02-24 02:41:31 +0700

    fix: delete api log from dashboard with modal confirmation

--- resources/views/filament/widgets/api-log-management.blade.php
+++ resources/views/filament/widgets/api-log-management.blade.php
@@ -1,8 +1,11 @@
 <x-filament-widgets::widget>
-    <x-filament::section heading="Manajemen API Logs" icon="heroicon-o-cog-6-tooth" description="Aksi maintenance untuk tabel api_logs">
+    <x-filament::section heading="Manajemen API Logs" icon="heroicon-o-cog-6-tooth"
+        description="Aksi maintenance untuk tabel api_logs">
         <div class="flex flex-wrap gap-3">
             {{ $this->clearOldLogsAction }}
             {{ $this->clearAllLogsAction }}
         </div>
     </x-filament::section>
+
+    <x-filament-actions::modals />
 </x-filament-widgets::widget>
