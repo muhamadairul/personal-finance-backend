2bccd860c71a78b3376c2f14d1ec6f4543baf1b7
muhamadairul <akunpertamapublic@gmail.com>
2026-04-23 01:45:57 +0700

    feat: configure application bootstrapping with custom routing, middleware, and exception handling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->statefulApi();
         $middleware->api(append: [
             \App\Http\Middleware\ApiLogger::class,
